e49361db58e6bae775ea4ebec08205d02b6dce1d
Berkay Uludoğan <buludogan0@gmail.com>
2025-08-02 16:22:05 +0300

    entity configurations added

---
+++ Infrastructure/HepsiApi.Persistence/Configurations/BrandConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Bogus;
+using HepsiApi.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HepsiApi.Persistence.Configurations
+{
+    public class BrandConfiguration : IEntityTypeConfiguration<Brand>
+    {
+        public void Configure(EntityTypeBuilder<Brand> builder)
+        {
+            builder.Property(b => b.Name).HasMaxLength(256);
+
+            Faker faker = new("tr");
+
+
+            builder.HasData(
+            new Brand()
+            { Id = 1, Name = faker.Commerce.Department(), CreatedDate = DateTime.Now, IsDeleted = false },
+            new Brand()
+            { Id = 2, Name = faker.Commerce.Department(), CreatedDate = DateTime.Now, IsDeleted = false }, 
+            new Brand()
+            { Id = 3, Name = faker.Commerce.Department(), CreatedDate = DateTime.Now, IsDeleted = false },
+            new Brand()
+            { Id = 4, Name = faker.Commerce.Department(), CreatedDate = DateTime.Now, IsDeleted = false });
+        }
+    }
+}
