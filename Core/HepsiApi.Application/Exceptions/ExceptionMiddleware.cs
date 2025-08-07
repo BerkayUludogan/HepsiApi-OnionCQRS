5ce9ac671926ac36b1915f0ac0c1833f7c3695c1
Berkay Uludoğan <buludogan0@gmail.com>
2025-08-07 19:35:47 +0300

    global exception handling finish

---
+++ Core/HepsiApi.Application/Exceptions/ExceptionMiddleware.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using SendGrid.Helpers.Errors.Model;
+using System.ComponentModel.DataAnnotations;
+
+namespace HepsiApi.Application.Exceptions
+{
+    public class ExceptionMiddleware : IMiddleware
+    {
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsnyc(context, ex);
+            }
+        }
+        private static Task HandleExceptionAsnyc(HttpContext context, Exception exception)
+        {
+            var statusCode = GetStatusCode(exception);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = statusCode;
+
+            List<string> errors = new()
+            {
+                $"Hata Mesajı: {exception.Message}",
+                $"Mesaj Açıklaması: {exception.InnerException}", 
+            };
+            return context.Response.WriteAsync(text: new ExceptionModel
+            {
+                Errors = errors,
+                StatusCode = statusCode
+            }.ToString());
+        }
+        private static int GetStatusCode(Exception exception) =>
+            exception switch
+            {
+                BadRequestException => StatusCodes.Status400BadRequest,
+                NotFoundException => StatusCodes.Status404NotFound,
+                ValidationException => StatusCodes.Status422UnprocessableEntity,
+                _ => StatusCodes.Status500InternalServerError
+            };
+    }
+}
