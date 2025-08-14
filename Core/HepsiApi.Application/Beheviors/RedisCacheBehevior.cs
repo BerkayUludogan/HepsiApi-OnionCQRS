6a94b46664ba126601245d74fe5df3bed74f220e
Berkay Uludoğan <buludogan0@gmail.com>
2025-08-14 13:02:07 +0300

    redis cache structure was established.

---
+++ Core/HepsiApi.Application/Beheviors/RedisCacheBehevior.cs
@@ -0,0 +1,39 @@
+﻿using HepsiApi.Application.Interfaces.RedisCache;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HepsiApi.Application.Beheviors
+{
+    public class RedisCacheBehevior<IReguest, TResponse> : IPipelineBehavior<IReguest, TResponse>
+    {
+        private readonly IRedisCacheService redisCacheService;
+
+        public RedisCacheBehevior(IRedisCacheService redisCacheService)
+        {
+            this.redisCacheService = redisCacheService;
+        }
+        public async Task<TResponse> Handle(IReguest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+        {
+            if (request is ICacheableQuery query)
+            {
+                var cacheKey = query.CacheKey;
+                var cacheTime = query.CacheTime;
+                
+                var cachedData = await redisCacheService.GetAsync<TResponse>(cacheKey);
+                if(cachedData is not null)
+                    return cachedData;
+
+                var response = await next();
+                if (response is not null)
+                    await redisCacheService.SetAsync(cacheKey, response, DateTime.Now.AddMinutes(cacheTime));
+
+                return response;
+            }
+            return await next();
+        }
+    }
+}
---
+++ Infrastructure/HepsiApi.Infrastructure/RedisCache/RedisCacheSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace HepsiApi.Infrastructure.RedisCache
+{
+    public class RedisCacheSettings
+    {
+        public string ConnectionString { get; set; }
+        public string InstanceName { get; set; }
+    }
+}
